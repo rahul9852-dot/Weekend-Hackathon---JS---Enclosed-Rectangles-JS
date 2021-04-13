ea2e4215c051846e709954f2e32de15388a09c2f
rahul9852-dot <66193581+rahul9852-dot@users.noreply.github.com>
2021-04-14 04:10:38 +0530

    udate

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,42 @@
 
 function updateStructure(rec1,rec2){
 	//write your code
+	
+	if(contains(rec1,rec2)){
+		
+		const relativeDim=relative(recA,recB);
+		return {...rec1,children:[relativeDim]}
+	}
+	else if(contains(rec2, rec1)){
+		const relativeDim=relative(rec2,rec1);
+		return {...rec2, children:[relativeDim]}
+	}
+	else{
+		return {...recA}
+	}
+}
+
+
+function relative(rec1,rec2){
+	const recAn=normalise(rec1)
+	const recBn=normalize(rec2)
+	
+	const res={
+		children:rec2.children
+	}
+	
+	if(rec2.top){
+		res.top=`{recBn.x1-recAn.x1}px`;
+	}
+	if(rec2.left){
+		res.left=`{recBn.y1-recAn.y1}px`;
+	}
+	if(rec2.height){
+		res.height=rec2.height;
+	}
+	if(rec2.width){
+		
+	}
 }
 
 module.exports = updateStructure;
